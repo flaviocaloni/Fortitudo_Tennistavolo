4593150a2165308fb77f3a3f0ca920987ae326da
emawesley10 <emanueleleto84@gmail.com>
2026-08-15 23:47:05 +0200

    Update 0017_add_queueusers.sql

--- supabase/migrations/0017_add_queueusers.sql
+++ supabase/migrations/0017_add_queueusers.sql
@@ -1,6 +1,9 @@
-** Controlla se il numero massimo di prenotazioni è stato raggiunto
-** Se sì, invece di bloccare l’utente → lo inserisce in una coda di attesa
-** Quando si libera un posto → il primo in coda viene promosso automaticamente alla prenotazione
+-- =======================================================================
+-- CONTROLLA SE IL NUMERO MASSIMO DI PRENOTAZIONI E' STATO RAGGIUNTO
+-- SE SI, ANZICHE' BLOCCARE L'UTENTE → LO INSERISCE IN UNA CODA DI ATTESA
+-- QUANDO SI LIBERA UN POSTO → IL PRIMO IN CODA VIENE PROMOSSO 
+-- AUTOMATICAMENTE ALLA PRENOTAZIONE 
+-- =======================================================================
 
 ALTER TABLE public.training_slots
 ADD COLUMN queue uuid[] DEFAULT '{}';
