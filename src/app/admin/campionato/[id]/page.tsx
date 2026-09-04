6b37ba9a09d561d00ffe03d6f906d89f50b7036d
Flavio Caloni <flavio.caloni@gmail.com>
2026-09-04 11:28:20 +0200

    feat: add Partite tab to championship detail page for easy access

--- src/app/admin/campionato/[id]/page.tsx
+++ src/app/admin/campionato/[id]/page.tsx
@@ -144,7 +144,12 @@ export default async function AdminChampionatoDetailPage({ params }: PageProps) 
         <button className="px-4 py-2 border-b-2 border-blue-600 text-blue-600 font-semibold">
           Squadre
         </button>
-        {/* TODO: Aggiungi tab Partite, Calendario */}
+        <a
+          href={`/admin/campionato/${championshipId}/partite`}
+          className="px-4 py-2 border-b-2 border-transparent text-gray-600 hover:text-gray-900 hover:border-gray-300 transition"
+        >
+          Partite
+        </a>
       </div>
 
       {/* CREATE TEAM FORM */}
